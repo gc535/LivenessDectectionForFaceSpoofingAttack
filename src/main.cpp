5389abf7315ae25e5d096156cbfcf1e2971ec4f1
ubuntu <gc535@cornell.edu>
2018-10-11 18:39:01 -0600

    added 'test only' feature

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,19 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/ml.hpp>
 
+#include <main.hpp>
+#include <Data.hpp>
+#include <ProgressBar.hpp>
+#include <Util.hpp>
+
 #include <stdlib.h>
 #include <vector>
 #include <string>
 #include <iostream>
+#include <errno.h>
 
 
-
-#include <main.hpp>
-#include <Data.hpp>
-#include <ProgressBar.hpp>
+//#include <Util.hpp>
 
 void test(cv::Mat, cv::Mat, cv::Ptr<cv::ml::SVM>);
 
@@ -18,6 +21,7 @@ int main(int argc, char** argv)
 {
 	int resize = 96;
 	int cellsize = 16;
+	Action action = TRAIN;
 	std::string dataDir = std::string("..");
 
 	if( argc <= 2 ) {
@@ -39,6 +43,23 @@ int main(int argc, char** argv)
 				cellsize = atoi( argv[i + 1] );
 				i++;
 			}
+			else if( strcmp( argv[i], "-a" ) == 0 ) {
+				if( strcmp( argv[i+1], "test" ) == 0 )
+				{
+					action = TEST;
+				}
+				else if( strcmp( argv[i+1], "train" ) == 0 )
+				{
+					action = TRAIN;
+				}
+				else
+				{
+					std::cout<<"[ERROR]: Invalid action. Action can only be 'train' or 'test'."<<std::endl;
+					std::cout<<"\t\t\t Execution aborted."<<std::endl;
+					exit( 1 );
+				}
+				i++;
+			}
 			else {
 				std::cerr << "invalid argument: \'" << argv[i] << "\'\n";
 				printHelp();
@@ -50,36 +71,52 @@ int main(int argc, char** argv)
 	std::string testDir = dataDir+"/test/";
 	std::string tmpDir = dataDir+"/tmp/";
 
-	
+	std::cout<<"[Note]: Starting data preparation phase..."<<std::endl;
 	// prepare train data
 	Data data(trainDir, Data::Action::TRAIN, resize, cellsize);
 	cv::Mat train_data, train_label;
-	data.DataPreparation(train_data, train_label);
-
+	if(action != TEST)
+	{
+		data.DataPreparation(train_data, train_label);
+	}
 	
+	// prepare test data
 	data.update(testDir, Data::Action::TEST, resize, cellsize);
 	cv::Mat test_data, test_label;
 	data.DataPreparation(test_data, test_label);
 
-	// training 
 
+	std::cout<<"[Note]: Starting model traning phase..."<<std::endl;
 	cv::Ptr<cv::ml::SVM> svm = cv::ml::SVM::create();
-  	svm->setType(cv::ml::SVM::Types::C_SVC);
-  	svm->setKernel(cv::ml::SVM::KernelTypes::LINEAR);
-  	svm->setTermCriteria(cv::TermCriteria(cv::TermCriteria::MAX_ITER,100,1e-6));
-  	std::cout << "[Note]: Training SVM......" << std::endl;
-  	try 
-  	{
-		//svm = StatModel::train<SVM>(train_feature, ROW_SAMPLE, train_label, params);//train SVM
-		svm->train(train_data,cv::ml::SampleTypes::ROW_SAMPLE,train_label);
-  		std::cout << "[Note]: Training finished......" << std::endl;
-  		svm->save("liveness_svm.xml");
-  	} 
-  	catch (cv::Exception& e) 
-  	{
-		std::cout << e.msg;
-  	}
+	// training 
+	if(!exists("liveness_svm.xml"))
+	{
+		svm->setType(cv::ml::SVM::Types::C_SVC);
+	  	svm->setKernel(cv::ml::SVM::KernelTypes::LINEAR);
+	  	svm->setTermCriteria(cv::TermCriteria(cv::TermCriteria::MAX_ITER,100,1e-6));
+	  	std::cout << "[Note]: Training SVM......" << std::endl;
+	  	try 
+	  	{
+			//svm = StatModel::train<SVM>(train_feature, ROW_SAMPLE, train_label, params);//train SVM
+			svm->train(train_data, cv::ml::SampleTypes::ROW_SAMPLE, train_label);
+	  		std::cout << "[Note]: Training finished......" << std::endl;
+	  		svm->save("liveness_svm.xml");
+	  	} 
+	  	catch (cv::Exception& e) 
+	  	{
+			std::cout << e.msg;
+	  	}
+	}
+	else
+	{
+		std::cout << "[Note]: Pre-trained SVM model found in the current directory, load it from file......" << std::endl;
+		svm = cv::Algorithm::load<cv::ml::SVM>("liveness_svm.xml");
+	}
+  	std::cout<<"[Note]: Model traning phase complete!"<<std::endl;
 
+
+	std::cout<<"[Note]: Starting model testing phase..."<<std::endl;
+  	std::cout<<"[Note]: Test set size: "<<test_data.rows<<" samples"<< std::endl;
   	test(test_data, test_label, svm);
 
     return 0;
--- src/main.hpp
+++ src/main.hpp
@@ -2,10 +2,12 @@
 #define MAIN_HPP
 
 
-//#define resize_row 128
-//#define resize_col 96
-//#define cell_size 16
 
+enum Action
+{
+	TRAIN,
+	TEST
+};
 
 
 void printHelp() {
@@ -16,12 +18,5 @@ void printHelp() {
 	std::cout << "\t-d <string> - Specificy a path to the root of data directory: (default=none)" << std::endl;
 	std::cout << "\t \t \tData root should contain a two sub folder: 'train' and 'test" << std::endl;
 }
-/*
-enum Action
-{
-	TRAIN,
-	TEST
-};
-*/
 
 #endif //MAIN_HPP
