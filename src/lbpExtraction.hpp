904a8326208479ce4d19399699b73382568eab55
ubuntu <gc535@cornell.edu>
2018-10-25 15:04:51 -0600

    moved the printHelp definition into cpp

--- src/lbpExtraction.hpp
+++ src/lbpExtraction.hpp
@@ -10,13 +10,6 @@ enum Action
 };
 
 
-void printHelp() {
-	std::cout << "\nUsage: ./main [options]" << std::endl;
-	std::cout << "\nOptions:" << std::endl;
-	std::cout << "\t-r <int> - Target resize size (default=96)" << std::endl;
-	std::cout << "\t-c <int> - Desired cell size for LBP extraction (default=16)" << std::endl;
-	std::cout << "\t-d <string> - Specificy a path to the root of data directory: (default=none)" << std::endl;
-	std::cout << "\t \t \tData root should contain a two sub folder: 'train' and 'test" << std::endl;
-}
+void printHelp();
 
 #endif //LBP_EXTRACTION_HPP
